b0993eab4548ed6bf705d97df7079c1e5dc53b3c
Vitaliy <vitaliy@smarthomecontrol.me>
2019-07-02 16:56:25 +0300

    init

---
+++ src/main/index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    width: 1080,
+    minWidth: 680,
+    height: 840,
+    title: app.getName(),
+    useContentSize: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.maximize()
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
